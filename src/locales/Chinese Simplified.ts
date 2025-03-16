5ed0cf166499b98c15740661d3b90f9829169c17
YasaitoriOvO <YasaitoriOvO@users.noreply.github.com>
2025-03-16 14:39:54 +0000

    [create-pull-request] automated change

--- src/locales/Chinese Simplified.ts
+++ src/locales/Chinese Simplified.ts
@@ -1,32 +1,32 @@
 export default {
   SendVerificationCode: {
-    mailTitle: "KIRAKIRA - 注册验证码",
-    mailLine0: "你的注册验证码是：",
-    mailLine1: "欢迎来到 KIRAKIRA。你可以使用这个验证码来注册你的账户。",
-    mailLine2: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。"
+    mailTitle: "KIRAKIRA - Registration Verification Code",
+    mailLine0: "Your registration verification code is: ",
+    mailLine1: "Welcome to KIRAKIRA. You can use this verification code to register your account.",
+    mailLine2: "Verification code is valid for 30 minutes. Please ensure do not disclose your verification code to others.<br><br>To stop receiving notifications, please contact the KIRAKIRA support team."
   },
   SendChangeEmailVerificationCode: {
-    mailTitle: "KIRAKIRA - 更改邮箱验证码",
-    mailLine0: "你更改邮箱的验证码是：",
-    mailLine1: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。",
-    mailLine2: " "
+    mailTitle: "KIRAKIRA - Change Email Verification Code",
+    mailLine0: "Your change email verification code is: ",
+    mailLine1: "Verification code is valid for 30 minutes. Please ensure do not disclose your verification code to others.",
+    mailLine2: "<br><br>To stop receiving notifications, please contact the KIRAKIRA support team."
   },
   SendChangePasswordVerificationCode: {
-    mailTitle: "KIRAKIRA - 更改密码验证码",
-    mailLine0: "你更改密码的验证码是：",
-    mailLine1: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。",
-    mailLine2: " "
+    mailTitle: "KIRAKIRA - Change Password Verification Code",
+    mailLine0: "Your change password verification code is: ",
+    mailLine1: "Verification code is valid for 30 minutes. Please ensure do not disclose your verification code to others.",
+    mailLine2: "<br><br>To stop receiving notifications, please contact the KIRAKIRA support team."
   },
   UserEmailAuthenticator: {
-    mailTitle: "KIRAKIRA - 验证 2FA 的验证码",
-    mailLine0: "验证 2FA 的验证码是：",
-    mailLine1: "注意：你可以使用这个验证码来验证你的 2FA（二重身份验证器）。",
-    mailLine2: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。"
+    mailTitle: "KIRAKIRA - Verification Code For Verifying 2FA",
+    mailLine0: "Your verification code for verifying Authenticator is: ",
+    mailLine1: "Note: Please make sure you will use this verification code to verify your Authenticator.",
+    mailLine2: "Verification code is valid for 30 minutes. Please ensure do not disclose your verification code to others.<br><br>To stop receiving notifications, please contact the KIRAKIRA support team."
   },
   DeleteUserEmailAuthenticator: {
-    mailTitle: "KIRAKIRA - 删除 2FA 的验证码",
-    mailLine0: "删除 2FA 的验证码是：",
-    mailLine1: "注意：你可以使用这个验证码来删除你的 2FA（二重身份验证器）。",
-    mailLine2: "验证码 30 分钟内有效。请注意安全，不要向他人泄露你的验证码。"
+    mailTitle: "KIRAKIRA - Verification Code for Deleting 2FA",
+    mailLine0: "Your verification code for deleting Authenticator is: ",
+    mailLine1: "Note: Please make sure you will use this verification code to delete your Authenticator.",
+    mailLine2: "Verification code is valid for 30 minutes. Please ensure do not disclose your verification code to others.<br><br>To stop receiving notifications, please contact the KIRAKIRA support team."
   }
 };
